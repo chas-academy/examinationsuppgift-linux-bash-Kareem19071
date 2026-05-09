4e7dd5f1f29fbf542e59070c5b8e0fe7c95411e7
Kareem19071 <kareemalmonajed07@gmail.com>
2026-05-09 16:49:24 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Måste vara root"
+  exit 1
+fi
+
+for USERNAME in "$@"
+do
+  useradd -m "$USERNAME"
+
+  mkdir -p /home/$USERNAME/Documents
+  mkdir -p /home/$USERNAME/Downloads
+  mkdir -p /home/$USERNAME/Work
+
+  echo "Välkommen $USERNAME" > /home/$USERNAME/welcome.txt
+  cut -d: -f1 /etc/passwd >> /home/$USERNAME/welcome.txt
+
+  chown -R $USERNAME:$USERNAME /home/$USERNAME
+done#!/bin/bash
+
 echo "Hej"
